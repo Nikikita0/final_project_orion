2f67e7acf2529f67c312d17a1db96531ca94fc9a
admin <suharevnikita161201@gmail.com>
2023-11-18 16:26:32 +0200

    complete

--- tests/test_order_page.py
+++ tests/test_order_page.py
@@ -1,16 +1,26 @@
 import pytest
 from ..pages.base_page import BasePage
 from ..pages.order_page import OrderPage
-from ..pages.login_page import LoginPage
+from ..pages.signup_page import SignupPage
 from ..settings import sets
+import random as rnd
 
 
 @pytest.mark.smoke
 @pytest.mark.regression
 @pytest.mark.order_page
 class TestOrderPage:
+
     def setup_method(self):
-        pass
+        hash_name = "%032x" % rnd.getrandbits(128)
+        self.email_for_signup = f"{hash_name}@mail.com"
+        self.password_for_signup = "123654789"
+        self.name_for_signup = 'Андрійов Андрій Андрійович'
+        phone = ''
+        for i in range(10): phone += str(rnd.randint(1, 9))
+        self.phone_for_signup = phone
+        self.postcode_for_signup = '09103'
+        self.address_for_signup = 'Шевченка, 15'
 
     def test_get_main_page(self, browser):
         page = BasePage(browser, sets.URL)
@@ -18,18 +28,21 @@ class TestOrderPage:
 
     def test_login_cabinet(self, browser):
         self.link_to_cabinet = browser.current_url
-        page = LoginPage(browser, self.link_to_cabinet)
-        page.click_button_login()
+        page = SignupPage(browser, self.link_to_cabinet)
+        page.click_button_signup()
+        page.explicit_wait(5)
+        page.input_signup_data(self.email_for_signup, self.password_for_signup, self.name_for_signup,
+                               self.phone_for_signup, self.postcode_for_signup, self.address_for_signup)
+        page.explicit_wait(5)
+        page.newsletter_decline()
         page.explicit_wait(2)
-        page.login_cabinet(sets.TEST_EMAIL, sets.PASSWORD)
+        page.confirm_signup()
         page.explicit_wait(3)
-        page.confirm_login()
-        page.is_h1_cabinet()
         page = OrderPage(browser, self.link_to_cabinet)
         page.explicit_wait(3)
         page.go_to_main_after_login()
 
-    def add_products_to_cart(self, browser):
+    def test_add_product_to_cart(self, browser):
         self.link_to_cabinet = browser.current_url
         page = OrderPage(browser, self.link_to_cabinet)
         page.explicit_wait(3)
@@ -39,6 +52,11 @@ class TestOrderPage:
         page.explicit_wait(3)
         page.add_popular_product_to_cart()
         page.explicit_wait(3)
+        page.go_to_order()
+
+    def test_make_order(self, browser):
+        self.link_to_cabinet = browser.current_url
+        page = OrderPage(browser, self.link_to_cabinet)
         page.make_order()
         page.explicit_wait(3)
 
